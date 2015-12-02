d0b6c5119c1886b5f8e318285f5115e727f6e58d
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-12-02 18:09:31 +0000

    Initial skelton plugin code.

---
+++ pjw-groupme-importer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ Plugin Name: PJW GroupMe Importer
+ Plugin URI: https://github.com/westi/pjw-groupme-importer
+ Description: Lets you import from GroupMe
+ Author: westi
+ Version: 0.1-alpha
+ Author URI: http://blog.ftwr.co.uk/
+ */
