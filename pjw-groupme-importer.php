7633cf744b4134b029a81db0e596893e63535cfb
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-12-02 20:38:01 +0000

    Initial group listing version of the code, handles connection if you follow the link from the GroupMe dev panel.

--- pjw-groupme-importer.php
+++ pjw-groupme-importer.php
@@ -7,3 +7,12 @@
  Version: 0.1-alpha
  Author URI: http://blog.ftwr.co.uk/
  */
+ 
+require_once( __DIR__ . '/includes/class-pjw-gm-oauth.php' );
+require_once( __DIR__ . '/includes/class-pjw-groupme-rest-api.php' );
+
+function pjw_groupme_importer_go() {
+	global $pjw;
+	$pjw['pjw_groupme_oauth'] = new pjw_groupme_oauth();
+}
+add_action('plugins_loaded', 'pjw_groupme_importer_go' );
