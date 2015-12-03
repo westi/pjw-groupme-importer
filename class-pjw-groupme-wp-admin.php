5c8e26fcce343c9441ea97277e9b328292a6b455
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-12-03 21:31:05 +0000

    Add custom columns see #9

--- class-pjw-groupme-wp-admin.php
+++ class-pjw-groupme-wp-admin.php
@@ -2,6 +2,8 @@
 class pjw_groupme_wp_admin {
 	public function __construct() {
 		add_action( 'init', array( $this, 'register_post_type' ) );
+		add_filter( 'manage_pjw-groupme-message_posts_columns', array( $this, 'register_custom_post_type_columns' ) );
+		add_action( 'manage_pjw-groupme-message_posts_custom_column', array( $this, 'display_custom_post_type_columns' ), 10, 2 );
 	}
 
 	/**
@@ -21,6 +23,35 @@ class pjw_groupme_wp_admin {
 				'map_meta_cap' => true,
 			)
 		);
+	}
+
+	/**
+	 * Modify the list of columns that are displayed for posts in wp-admin.
+	 *
+	 * @param array $_columns
+	 * @return array
+	 */
+	public function register_custom_post_type_columns( $_columns ) {
+		unset( $_columns['title'] ); // We don't want the title displayed
+		unset( $_columns['date'] ); // Remove the default date position so we can rename and add at the end.
+		$_columns['pjw_groupme_name'] = 'User';
+		$_columns['pjw_groupme_message'] = 'Message';
+		$_columns['date'] = 'Date';
+		return $_columns;
+	}
+
+	/**
+	 * Output the content for the custom columns in wp-admin
+	 */
+	public function display_custom_post_type_columns( $_column_name, $_post_id ) {
+		switch( $_column_name ) {
+			case 'pjw_groupme_name':
+				echo get_post_meta( $_post_id, $_column_name, true );
+				break;
+			case 'pjw_groupme_message':
+				the_content();
+				break;
+		}
 	}
 }
 // Throw us into the wind
