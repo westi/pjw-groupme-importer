dbbbaf1f5613db2954f4dc57c53b6996bcb2f0f3
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-12-03 03:08:33 +0000

    Flesh out the WP api with a message sync method and a way to ID the last synced message.

--- class-pjw-groupme-wp-api.php
+++ class-pjw-groupme-wp-api.php
@@ -35,11 +35,47 @@ class pjw_groupme_wp_api {
 	}
 
 	public function get_last_synced_message_id( $group_id ) {
-		
+		$_message_ids = get_posts(
+			array(
+				'fields' => 'ids',
+				'numberposts' => 1,
+				'post_type' => 'pjw-groupme-message',
+				'meta_key' => 'pjw_groupme_group_id',
+				'meta_value' => $group_id,
+				'orderby' => 'id',
+				'order' => 'DESC'
+			)
+		);
+		if ( ! empty( $_message_ids ) ) {
+			return get_post_meta( $_message_ids[0], 'pjw_groupme_message_id', true );
+		} else {
+			return false;
+		}
 	}
 
 	public function sync_message( $message ) {
-		
+		$_post = array(
+			'post_type' => 'pjw-groupme-message',
+			'post_status' => 'publish',
+			'post_content' => $message->text,
+			'post_date' => date( 'Y-m-d H:i:s', $message->created_at ),
+			'post_date_gmt' => date( 'Y-m-d H:i:s', $message->created_at ),
+		);
+
+		$_post_meta = array(
+			'pjw_groupme_group_id' => $message->group_id,
+			'pjw_groupme_message_id' => $message->id,
+			'pjw_groupme_source_guid' => $message->source_guid,
+			'pjw_groupme_user_id' => $message->user_id,
+			'pjw_groupme_name' => $message->name,
+			'pjw_groupme_avatar_url' => $message->avatar_url,
+		);
+
+		$_post_obj = wp_insert_post( $_post );
+
+		foreach( $_post_meta as $_key => $_value ) {
+			add_post_meta( $_post_obj, $_key, $_value, true );
+		}
 	}
 }
 $pjw_groupme_wp_api = new pjw_groupme_wp_api();
